b70f1f0f810616d7af7de6cb6d8631ebc522fca0
( Sad8669 <41834162+saifkhan8669@users.noreply.github.com>
2022-06-23 19:21:09 -0700

    Performance improvements
    Cleaned up code and improved the algorithm.

--- WAVExtractor/Program.cs
+++ WAVExtractor/Program.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.IO;
-using System.Text;
+using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-
+using System.IO;
+using System.Diagnostics;
 
 namespace WAVExtractor
 {
@@ -11,133 +9,117 @@ namespace WAVExtractor
     {
         static void Main(string[] args)
         {
-
             Console.Clear();
-            Console.Title = "WAVExtractor";
+            Console.Title = "WAVExtractor | :(Sad8669";
 
+            if (args.Length != 2)
 
-
-            //////////////////////////////////////////////////////////////////////////////////////////
-            if (args.Length == 2)
             {
-
-                string fileName = Path.GetFileNameWithoutExtension(args[0]);
-                byte[] infile = File.ReadAllBytes(args[0]);
-
-
-                // Keeps in check the positions of headers.
-                List<int> position = new List<int>();
-
-
-                // Reading Loop : Just to find positions of headers..
-                PositionFinder(infile, position);
-             
-
-                if (position.Count == 0)
-
-                {
-                    Console.WriteLine("No audio was found, please check your input.");
-                }
-
-
-                WAVMethods.DirectoryCheck(args);
-                string[] dir = Directory.GetFiles(args[1]);
-
-
-                // Extraction Algorithm : (Check 'MethodsForWAVExtractor.cs' for more info)
-                ExtractingWAVFiles EWF = new ExtractingWAVFiles(position, infile, args, fileName);
-
-                Console.WriteLine();
-                Console.WriteLine("WAV/WEM - Extractor | :(Sad8669 / Saif Ullah");
-
-
+                IO.Usage();
+                Environment.Exit(1);
             }
 
+            Stopwatch timecountDetect = new Stopwatch();
 
-            //////////////////////////////////////////////////////////////////////////////////////
-            else if (args.Length == 3 && args[2] == "-r")
+            IO.FileExistCheck(args);
+            IO.DirectoryExistCheck(args);
+
+            // Input File
+            var bigStream = File.Open(args[0], FileMode.Open, FileAccess.Read);
+
+
+            // Variables and Magic Numbers //
+            // ---------------------------------------------- //
+            int currentByte;
+            byte[] buffer = new byte[4]; // Window
+            byte[] pattern = { 82, 73, 70, 70 }; // RIFF
+            byte[] waveFormat = { 87, 65, 86, 69 }; // WAVE
+            byte[] fileSize = new byte[4]; // TBD
+            // ---------------------------------------------- //
+
+            // To save streams position
+            List<long> position = new List<long>();
+
+
+            timecountDetect.Start();
+
+
+
+            while ((currentByte = bigStream.ReadByte()) != -1)
+
             {
-                string fileName = Path.GetFileNameWithoutExtension(args[0]);
-                byte[] infile = File.ReadAllBytes(args[0]);
 
-
-                // Keeps in check the positions of headers.
-                List<int> position = new List<int>();
-
-                position.Add(0);
-
-
-                // Reading Loop : Just to find positions of headers..
-
-
-                if (position.Count == 0)
+                for (int i = 0; i < 3; i++)
 
                 {
-                    Console.WriteLine("No audio was found, please check your input.");
+
+
+                    buffer[i] = buffer[i + 1];
                 }
 
-                // Checks if "<output path> exist, if not then it creates the path. (Check 'MethodsForWAVExtractor.cs' for more info)
-                WAVMethods.DirectoryCheck(args);
-                string[] dir = Directory.GetFiles(args[1]);
+                buffer[3] = (byte)currentByte;
 
-                // Reimporting Algorithm : (Check 'MethodsForWAVExtractor.cs' for more info)
-                for (int k = 1; k < position.Count; k++)
+
+
+                if (IO.HeaderEquals(buffer, pattern))
 
                 {
-
-                    int length = position[k] - position[k - 1]; // Absolute length of the WAV Audio.
-
-                    foreach (string file in dir)
+                    if (IO.IsWave(buffer, waveFormat, bigStream))
 
                     {
+                        // Resets the position
+                        bigStream.Position -= 8;
+                        Console.WriteLine("-> [0] WAVE stream located at offset(d): " + (bigStream.Position - buffer.Length));
+                        position.Add(bigStream.Position - buffer.Length);
 
-                        /*byte[] smallFile = File.ReadAllBytes(file);
-                        Array.Copy(smallFile, 0, infile, position[k - 1], length);
-                        File.WriteAllBytes(args[0], infile);*/
+                        fileSize[0] = (byte)bigStream.ReadByte();
+                        fileSize[1] = (byte)bigStream.ReadByte();
+                        fileSize[2] = (byte)bigStream.ReadByte();
+                        fileSize[3] = (byte)bigStream.ReadByte();
 
-                        Console.WriteLine(file);
 
+                        bigStream.Position += BitConverter.ToInt32(fileSize, 0);
                     }
 
-                }
+                    else
 
-
-            }
-
-            /////////////////////////////////////////////////////////////////////////////////////////
-            else
-
-            {
-                WAVMethods.Help();
-            }
-
-        }
-
-        public static void PositionFinder(byte[] infile, List<int> position)
-        {
-            for (int i = 0; i < infile.Length - 15; i++)
-
-            {
-
-                // It's easy to read in strings.
-                string headerType = Encoding.ASCII.GetString(infile[i..(i + 4)]);
-                string subChunk = Encoding.ASCII.GetString(infile[(i + 8)..(i + 15)]);
-
-
-                if (headerType == "RIFF" && subChunk == "WAVEfmt")
-
-                {
-
-                    position.Add(i);
-                    Console.WriteLine("-> RIFF Header Found at offset(d): " + i);
-
+                    {
+                        continue;
+                    }
                 }
 
             }
 
-            position.Add(infile.Length);
-            
+            IO.NoWAVECaseCheck(position, args);
+
+
+
+            position.Add(bigStream.Length);
+            bigStream.Close();
+
+            byte[] infile = File.ReadAllBytes(args[0]);
+           
+            for (int k = 1; k < position.Count; k++)
+
+            {
+                long length = position[k] - position[k - 1];
+                byte[] outfile = new byte[length];
+                Array.Copy(infile, position[k - 1], outfile, 0, length);
+                File.WriteAllBytes(args[1] + @"\" + Path.GetFileNameWithoutExtension(args[0]) + "_" + k + ".wav", outfile);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("File: " + Path.GetFileNameWithoutExtension(args[0]) + "_" + k + ".wav" + "| Status: Extracted! | Length: " + length);
+                Console.ResetColor();
+
+            }
+
+
+            timecountDetect.Stop();
+
+            Console.WriteLine("This process took " + timecountDetect.ElapsedMilliseconds / 1000.0 + " seconds!");
+            Console.WriteLine();
+            Console.WriteLine(":(Sad8669");
+
+
         }
     }
 }
-
